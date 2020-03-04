7d87f8f5ec98b46a75acbe3375500e74a7e98358
Tina <longevity.tina@gmail.com>
2020-03-04 12:23:58 -0800

    add game over and start screen

--- js/main.js
+++ js/main.js
@@ -1,22 +1,41 @@
 
 const init = () => {
     let score = 0
+    // REMOVE .gameStart node from HTML
+    const gameStart = document.querySelector('.gameStart')
+    gameStart.remove()
+
+
     //RENDER FISHES FROM OBJECT
     fishLayout()
 
     //TOGGLE
     setInterval(() => {
         //to use numbers as IDs format: '#\\3 ID'
-        document.querySelector('#\\31 ').classList.toggle('movedB')
-        document.querySelector('#\\30 ').classList.toggle('movedUp')
+        let item = document.querySelector('#\\31 ')
+        let itemTwo = document.querySelector('#\\30 ')
+        // if item is existing in DOM, then continue with toggle
+        if (item) {
+            document.querySelector('#\\31 ').classList.toggle('movedB')
+        }
+        if (itemTwo) {
+            document.querySelector('#\\30 ').classList.toggle('movedUp')
+        }
     }, 1000)
 
     setInterval(() => {
-        document.querySelector('#\\32 ').classList.toggle('moved')
+
+        let item = document.querySelector('#\\32 ')
+        if (item) {
+            document.querySelector('#\\32 ').classList.toggle('moved')
+        }
     }, 2000)
 
     setInterval(() => {
-        document.querySelector('#\\33 ').classList.toggle('movedShark')
+        let item = document.querySelector('#\\33 ')
+        if (item) {
+            document.querySelector('#\\33 ').classList.toggle('movedShark')
+        }
     }, 2000)
 }
 
@@ -52,6 +71,35 @@ const fishes = [
         image: 'images/shark.png'
     }
 ]
+//FISH LVL 2
+// const fishesTwo = [
+//     {
+//         fishName: '1',
+//         fishType: document.querySelector('.fish'),
+//         score: 100,
+//         image: 'images/fish1.png'
+//     },
+//     {
+//         fishName: '2',
+//         fishType: document.querySelector('.fish2'),
+//         score: 200,
+//         image: 'images/fish art2.png'
+//     },
+//     {
+//         fishName: '3',
+//         fishType: document.querySelector('.fish3'),
+//         score: 300,
+//         image: 'images/fish art3.png'
+//     },
+//     {
+//         fishName: 'shark',
+//         fishType: document.querySelector('.shark'),
+//         score: 0,
+//         image: 'images/shark.png'
+//     }
+// ]
+
+
 
 //SHUFFLE FISH ARRAY(IN PLACE)
 function shuffle(array) {
@@ -102,47 +150,67 @@ function handleFish(event) {
         } else if (fish === '3') {
             console.log(`${fishes[3].score}`)
             score = 0
+            gameOver()
         }
         document.querySelector('.score').innerHTML = score
-        clickedFish(fishSource)
+        removeFish(fishSource)
 
     }
 }
 // RENDER FISH
+
 function fishLayout() {
     fishes.forEach(function (fish, idx) {
-        const fishElement = document.createElement('img')
+        let fishElement = document.createElement('img')
         fishElement.setAttribute('src', fish.image)
         fishElement.setAttribute('id', idx)
         document.querySelector('.container').appendChild(fishElement)
-
-
     })
 }
 
 
-function clickedFish(srcElement) {
+function removeFish(srcElement) {
     srcElement.remove()
-    //if fish image is clicked(get attribute)
-    // if (fish.)
-    // then remove/hide, set attribute ===none
-
 }
+
+// * Game Over screen = eaten
+function gameOver() {
+    const gameOver = document.createElement('div')
+    gameOver.classList.add('gameOver')
+    gameOver.innerHTML = "Game Over"
+
+
+
+
+
+    //add 'gameover' as a child of container
+    const container = document.querySelector('.container')
+    container.appendChild(gameOver)
+}
+
+/**
+ * Store clicked fish into empty array
+ * if full then empty fishTwo array into game
+ */
+const deadFishes = []
+
+
 
 
 /***
  * TODO
- * images disappear after clicking
  * more fishes appear that are harder to click
  *  to replace original fish
  * More sharks appear
  *
- * Game Over screen = eaten
- * game over screen = time runs out
- * Game start screen with instructions
  *
  *timer
 clicking outside of images = lose points
 
  *fishes dive into water
+
+ add more fishes to array(or have a lvl 2 array),
+ distribute only some of fishes at first,
+ then distribute more as game goes on?
+ need to have remainder to speed up transitions
  */
